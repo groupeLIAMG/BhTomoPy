79a7e0e15b0ff14125f4e17e13755f8f0885ac56
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2016-08-11 09:47:37 -0400

    minor edit in raytracing code + some code cleanup

--- setup.py
+++ setup.py
@@ -9,17 +9,17 @@ from Cython.Build import cythonize
 
 extensions = [
     Extension('cutils.cgrid2d',
-                 sources=['./cutils/cgrid2d.pyx', './cutils/Grid2Dttcr.cpp'],  # additional source file(s)
-                 include_dirs=['./cutils/',np.get_include()],
-                 language='c++',             # generate C++ code
-                 extra_compile_args=['-std=c++11'],),
+              sources=['./cutils/cgrid2d.pyx', './cutils/Grid2Dttcr.cpp'],  # additional source file(s)
+              include_dirs=['./cutils/',np.get_include()],
+              language='c++',             # generate C++ code
+              extra_compile_args=['-std=c++11'],),
     Extension('cutils.segy',
-                 sources=['./cutils/segy.pyx', './cutils/csegy.c'],  # additional source file(s)
-                 include_dirs=['./cutils/',np.get_include()],
-		 extra_compile_args=['-std=c99'],),
+              sources=['./cutils/segy.pyx', './cutils/csegy.c'],  # additional source file(s)
+              include_dirs=['./cutils/',np.get_include()],
+              extra_compile_args=['-std=c99'],),
 ]
 
 setup(
     name='cutils',
-    ext_modules = cythonize(extensions),
+    ext_modules = cythonize(extensions, include_path=['./cutils/',]),
 )
